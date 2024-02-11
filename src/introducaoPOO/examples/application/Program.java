1b9d4da47798eb3b53691660434f77df7a1b333a
a-dornez <142704596+a-dornez@users.noreply.github.com>
2024-02-11 16:15:56 -0300

    feature: added coded along example 'Program.java'
    illustrates modularization, methods creation and OOP for problem solving

---
+++ src/introducaoPOO/examples/application/Program.java
@@ -0,0 +1,44 @@
+package introducaoPOO.examples.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import introducaoPOO.examples.entities.Product;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		Product product = new Product();
+		
+		System.out.println("Enter product data:");
+		System.out.print("Name: ");
+		 product.name = scanner.nextLine();
+		System.out.print("Price: ");
+		 product.price = scanner.nextDouble();
+		System.out.print("Quantity: ");
+		 product.quantity = scanner.nextInt();
+		
+		
+		//System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+		System.out.println("\nProduct data: " + product);
+		
+		System.out.print("\nEnter the number of products to be aded in stock: ");
+		 int quantity = scanner.nextInt();
+		 product.addProducts(quantity);
+		
+		System.out.println("\nUpdated data: " + product);
+		
+		System.out.print("\nEnter the number of products to be removed from stock: ");
+		 quantity = scanner.nextInt();
+		 product.removeProducts(quantity);
+		
+		System.out.println("\nUpdated data: " + product);
+		
+		scanner.close();
+	}
+}
+
