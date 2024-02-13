b67dce30d829890fe1bb0b70c05df47508c114cd
a-dornez <142704596+a-dornez@users.noreply.github.com>
2024-02-13 11:49:50 -0300

    refactor:simplified 'SalaryReadjust' code and optimized solution based on instructor's approach
    feature: added toString() to print employee's data instead of using sysouts. This usage was entirely thought by instructor

---
+++ src/introducaoPOO/exercises/application/SalaryReadjust.java
@@ -0,0 +1,51 @@
+/* "Make a program that reads an employee's data (name, gross salary and tax). Then, show their data (name and net salary.
+ * Finally, raise their salary based on a given percentage (only gross salary is affected by such percentage), show again 
+ * their name followed by readjusted salary. Use the following class model:
+ * 				________________________________________________
+ * 				|					 Employee					|
+ * 				|-----------------------------------------------|
+ * 				| - name: String								|
+ * 				| - GrossSalary: double							|
+ * 				| - tax: double									|
+ * 				|-----------------------------------------------|
+ * 				| + NetSalary(): double							|
+ * 				| + IncreaseSalary( percentage: double ) : void |
+ * 				|_______________________________________________|
+ * 
+ * 		-- Problem by Nelio Alves
+ */
+
+package introducaoPOO.exercises.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import introducaoPOO.exercises.entities.Employee;
+
+public class SalaryReadjust {
+	
+	public static void main (String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		Employee employee = new Employee();
+		
+		System.out.print("What is this employee's name: ");
+		 employee.name = scanner.nextLine();
+		System.out.print("What is this employee's gross salary?: ");
+		 employee.grossSalary = scanner.nextDouble();
+		System.out.print("What is the tax value deduced from their salary?: ");
+		 employee.tax = scanner.nextDouble();
+		
+		System.out.println("\nEmployee: " + employee);
+		
+		System.out.println("\nWhich percentage to increase salary?: ");
+		 employee.IncreaseSalary( scanner.nextDouble() );
+		
+		System.out.println("\nUpdated Data. Emloyee: " + employee);
+		
+		scanner.close();
+	}
+ 
+}
---
+++ src/introducaoPOO/exercises/entities/Employee.java
@@ -0,0 +1,22 @@
+package introducaoPOO.exercises.entities;
+
+public class Employee {
+	
+	public String name;
+	public double grossSalary;
+	public double tax;
+	
+	public double NetSalary() {
+		return grossSalary - tax;
+	}
+	
+	public void IncreaseSalary(double percentage) {
+		grossSalary += grossSalary * ( percentage / 100 );
+	}
+	
+	//this method was copied from instructor's solution. My approach was using a sysout in IncreaseSalary() method
+	public String toString() {
+		return name + ", $ " + String.format("%.2f", NetSalary() );
+	}
+
+}
