999d93c1a646ca6bfd0dcd16c325c04f8eb1b77d
a-dornez <142704596+a-dornez@users.noreply.github.com>
2024-02-18 21:21:28 -0300

    feature: added 'BankingOperations.java'
    executes an account registering, deposit and withdraw. Applies constructors, method overloading and attribute encapsulation

--- src/section9/exercises/application/BankingOperations.java
+++ src/section9/exercises/application/BankingOperations.java
@@ -1,5 +1,66 @@
+/*
+ * "In a bank, to register a bank account, it's necessary to inform an account number, it's holder name and an initial
+ * deposit value that the holder depositied while crating their account. That initial vakue, however, it's opitional,
+ * therefore: if account holder does not deposit any amount while registering their account, that account's initial
+ * value would be, naturally, zero.
+ *
+ * "Important: once created, an account number must not be changeable. Though an account holder's name may be modified
+ * (as a person's name may be changed after a marriage, for instance).
+ * 
+ * "Finally, an account's balance must not modified freely in an arbitrary way. There must be a mechanism to protect
+ * such a thing. An account's balance must only be increased due to deposits and only be diminished reduced due to
+ * withdraws. Consider that for each withdraw value the bank applies an $5.00 tax. Note: an account may get to a negative
+ * balance value if it's balance is not enough to perform a withdraw and/or pay the tax.
+ * 
+ * "You must make a program that executes an accounts registering process, allowing it's user to choose whether they
+ * want to make an initial deposit or not. Then, prompt user to make a deposit and then to make a withdraw, always 
+ * displaying updated data on account status after each operation.
+ * 
+ * 	-- Problem by Nelio Alves
+ */
+
 package section9.exercises.application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import section9.exercises.entities.Account;
+
 public class BankingOperations {
+	
+	public static void main (String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("\nWelcome\nPlease, enter account number: ");
+		 int accountID = scanner.nextInt() ;
+		System.out.print("Now, enter account holder's name: ");
+		 scanner.nextLine();
+		 String accountHoldersName = scanner.nextLine();
+		System.out.print("Is there a initial deposit?(y/n): ");
+		 String initialDepositChoice = scanner.nextLine();
+		 
+		double initialDeposit = 0;
+		if ( initialDepositChoice.equalsIgnoreCase("y") ) {
+			System.out.print("Enter initial deposit value: ");
+			 initialDeposit = scanner.nextDouble();
+		}
+		
+		Account account = new Account(accountID, accountHoldersName, initialDeposit);
+		
+		System.out.println("\n" + account.toString() );
+		
+		System.out.print("\nEnter a deposit value: ");
+		 account.Deposit( scanner.nextDouble() );
+		System.out.println("Updated account data: \n" + account.toString() );
+		
+		System.out.print("\nEnter a withdraw value: ");
+		 account.Withdrawl( scanner.nextDouble() );
+		System.out.println("Updated account data:\n" + account.toString() + "\n");
+		
+		scanner.close();
+	}
 
 }
+
--- src/section9/exercises/entities/Account.java
+++ src/section9/exercises/entities/Account.java
@@ -1,5 +1,54 @@
+/*
+ * Class created to apply constructors, method overloading and attributes encapsulation
+ */
+
 package section9.exercises.entities;
 
 public class Account {
-
+	
+	private int accountID;
+	private String accountHoldersName;
+	private double accountBalance;
+	private static final double WITHDRAWL_TAX = 5.00;
+	
+	public Account( int accountID, String accountHoldersName, double initialDeposit ) {
+		this.accountID = accountID;
+		this.accountHoldersName = accountHoldersName;
+		this.accountBalance = initialDeposit;
+	}
+	
+	public Account( int accountID, String accountHoldersName ) {
+		this.accountID = accountID;
+		this.accountHoldersName = accountHoldersName;
+	}
+	
+	public int getAccount() {
+		return accountID;
+	}
+	
+	public String getAccountHoldersName() {
+		return accountHoldersName;
+	}
+	
+	public void setAccountHoldersName( String accountHoldersName ) {
+		this.accountHoldersName = accountHoldersName;
+	}
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+	
+	public void Deposit( double depositValue ) {
+		accountBalance += depositValue;
+	}
+	
+	public void Withdrawl( double withdrawlValue ) {
+		accountBalance = accountBalance - withdrawlValue - WITHDRAWL_TAX;
+	}
+	
+	public String toString() {
+		return "Account " + accountID
+				+ ", Holder: " + accountHoldersName
+				+ ", Balance: $ " + String.format("%.2f", accountBalance);
+	}
 }
+
