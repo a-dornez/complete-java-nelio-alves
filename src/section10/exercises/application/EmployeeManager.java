f5d661fe45927ad89988f090401a274188e2a8a1
a-dornez <142704596+a-dornez@users.noreply.github.com>
2024-03-17 23:04:35 -0300

    refactor: set lambda operation at id validator method as definitive approach
    searches for input id and returns if is already being used

--- src/section10/exercises/application/EmployeeManager.java
+++ src/section10/exercises/application/EmployeeManager.java
@@ -39,16 +39,12 @@ public class EmployeeManager {
 		for ( int i = 0; i < employeesQuantity; i++ ) {
 			System.out.printf( "Employee #%d:\n", i + 1 );
 			System.out.print("Id: ");
-			Integer employeeId;
-			boolean validId;
+			Integer employeeId = scanner.nextInt();
 				//checks if inserted id already exists
-				do {
+				while ( IdRegisterValidator(employees, employeeId) == false) {
+					System.out.print("This Id already exists. Please insert another value: ");
 					employeeId = scanner.nextInt();
-					validId = IdRegisterValidator(employees, employeeId);
-					if (validId == false) {
-						System.out.print("This Id already exists, please insert another value: ");
-					}
-				} while ( validId == false ); 
+				}
 			System.out.print("Name: ");
 			scanner.nextLine();
 			String employeeName = scanner.nextLine();
@@ -63,45 +59,33 @@ public class EmployeeManager {
 		System.out.print("Enter the employee Id that will have a salary increase: ");
 		int employeeId = scanner.nextInt();
 		
+		
 		//validates inserted id and collects readjust rate if id is valid
 		Employee chosenEmployee = employees.stream().filter(x -> x.getEmployeeId() == employeeId ).findFirst().orElse(null);
-		if ( chosenEmployee != null ) {
-			System.out.print("Enter the readjust percentage: ");
-			double readjustRate = scanner.nextDouble();
-			
-			chosenEmployee.SalaryRaise(readjustRate);
-		}
-		else {
-			System.out.println("\nThis Id does not exist!");
-		}
+			if ( chosenEmployee != null ) {
+				System.out.print("Enter the readjust percentage: ");
+				double readjustRate = scanner.nextDouble();
+				
+				chosenEmployee.SalaryRaise(readjustRate);
+			}
+			else {
+				System.out.println("\nThis Id does not exist!");
+			}
 		
 		System.out.println("\nList of employees:");
-		for ( Employee employee : employees ) {
-			System.out.println(" " + employee.toString());
-		}
+			for ( Employee employee : employees ) {
+				System.out.println(" " + employee.toString());
+			}
 		
 		scanner.close();
 	}
 	
-
 	static boolean IdRegisterValidator ( List<Employee> employees, int employeeId ) {
-		/*
-		Employee chosenEmployee = employees.stream().filter(x -> x.getEmployeeId() == employeeId ).findFirst().orElse(null);
-		System.out.println(chosenEmployee);
-		if ( chosenEmployee != null ) {
-			return false;
-		}
-		else {
-			return true;
-		}
-		*/
 		
-		for ( int i = 0; i < employees.size(); i++ ) {
-			// System.out.println("Shazam!"); //signalizes loop execution, left as a memento of the joy when code started working
-			if ( employees.get(i).getEmployeeId() == employeeId ) {
-			//	System.out.println("Ahoy!"); //signalizes conditional execution, left as a memento of the joy when code finally started working
-				return false;
-			}
+		//checks if inserted id is available
+		Employee chosenEmployeeId = employees.stream().filter(x -> x.getEmployeeId() == employeeId ).findFirst().orElse(null);
+		if ( chosenEmployeeId != null ) {
+			return false;
 		}
 		
 		return true;
